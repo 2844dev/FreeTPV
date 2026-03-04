fe2df2282773b2626c2739efb15648e88d2e88bc
Mateo <mateodiezlopez@gmail.com>
2026-03-04 16:15:56 +0100

    Actualizar logo y añadimos logica para cambiar de menu en la pantalla principal

--- src/main/java/com/mateo/freetpv/controller/MainController.java
+++ src/main/java/com/mateo/freetpv/controller/MainController.java
@@ -1,4 +1,44 @@
 package com.mateo.freetpv.controller;
 
+import com.mateo.freetpv.HelloApplication;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+
+import java.io.IOException;
+
 public class MainController {
+    @FXML private Button ventasButton;
+    @FXML private Button productosButton;
+    @FXML private Button inventarioButton;
+    @FXML private StackPane contenedor;
+
+
+    private void cargarVista(String fxml) {
+        try {
+
+            // Creamos un fxmlloader con un archivo fxml indicado
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("view/" + fxml));
+
+            // Creamos un nuevo nodo donde cargamos el fxml
+            Node vista = fxmlLoader.load();
+
+            // Y cargamos el nodo en el contenedor
+            contenedor.getChildren().setAll(vista);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @FXML public void mostrarVentas() {
+        cargarVista("ventas-view.fxml");
+    }
+    @FXML public void mostrarProductos() {
+        cargarVista("productos-view.fxml");
+    }
+    @FXML public void mostrarInventario() {
+        cargarVista("inventario-view.fxml");
+    }
 }
