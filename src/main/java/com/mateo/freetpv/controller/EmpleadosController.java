c881064e7c8c155690b1be2effebf41d0ede7e8d
Mateo <mateodiezlopez@gmail.com>
2026-03-08 20:34:33 +0100

    Ajustamos los tipos de datos de la tableview y columns. Añadimos la logica para saber que mostrar en cada columna y cargamos los usuarios a la tabla. Añadimos los 2 tipos de roles al choicebox

--- src/main/java/com/mateo/freetpv/controller/EmpleadosController.java
+++ src/main/java/com/mateo/freetpv/controller/EmpleadosController.java
@@ -1,5 +1,8 @@
 package com.mateo.freetpv.controller;
 
+import com.mateo.freetpv.dao.UsuarioDAO;
+import com.mateo.freetpv.model.Usuario;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
@@ -7,18 +10,23 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.BorderPane;
 
+import java.util.List;
+
 public class EmpleadosController {
+    // Panel principal de empleados
     @FXML private BorderPane empleadosPane;
     @FXML private Button editarButton;
     @FXML private Button nuevoButton;
     @FXML private TextField buscarField;
-    @FXML private TableView empleadosTable;
-    @FXML private TableColumn nombreColumn;
-    @FXML private TableColumn rolColumn;
-    @FXML private TableColumn fechaColumn;
+    @FXML private TableView<Usuario> empleadosTable;
+    @FXML private TableColumn<Usuario, String> nombreColumn;
+    @FXML private TableColumn<Usuario, String> rolColumn;
+    @FXML private TableColumn<Usuario, String> fechaColumn;
 
+    // Panel extra de edicion y creacion de empleados
     @FXML private BorderPane nuevoempleadoPane;
     @FXML private TextField usuarioField;
     @FXML private PasswordField pinField;
@@ -26,7 +34,22 @@ public class EmpleadosController {
     @FXML private Button guardarButton;
     @FXML private Button cancelarButton;
 
+    private UsuarioDAO usuarioDAO = new UsuarioDAO();
+
     @FXML public void initialize() {
 
+        // Indicamos a cada columna que atributo de usuario mostrar
+        nombreColumn.setCellValueFactory(new PropertyValueFactory<>("nombre"));
+        rolColumn.setCellValueFactory(new PropertyValueFactory<>("rol"));
+        fechaColumn.setCellValueFactory(new PropertyValueFactory<>("fecha_creacion"));
+
+        // Creamos una lista de usuarios llamando al metodo de UsuarioDAO
+        List<Usuario> usuarios = usuarioDAO.obtenerUsuarios();
+
+        // Convertimos la lista a una observableList y lo ponemos como los items en la tabla
+        empleadosTable.setItems(FXCollections.observableList(usuarios));
+
+        // Añadimos los roles al choicebox para elegir
+        rolChoiceBox.setItems(FXCollections.observableArrayList("admin", "camarero"));
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.mateo.freetpv {
     requires javafx.fxml;
     requires java.sql;
 
+    opens com.mateo.freetpv.model to javafx.base;
     opens  com.mateo.freetpv.controller to javafx.fxml;
     opens com.mateo.freetpv to javafx.fxml;
     exports com.mateo.freetpv;
