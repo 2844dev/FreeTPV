84aa81fc7927cc5a75f39eed68a1fad28135e9fd
Mateo <mateodiezlopez@gmail.com>
2026-03-22 18:46:29 +0100

    Añadir AtlantaFX y Ikonli
    - Añadimos dependencias en module-info.java
    - Añadimos dependencias en pom.xml
    - Actualizamos versiones de JavaFX-Fxml y JavaFX-Controls (21.0.6 -> 26)

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,10 @@ module com.mateo.freetpv {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires atlantafx.base;
+    requires org.kordamp.ikonli.core;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.ikonli.fontawesome5;
 
     opens com.mateo.freetpv.model to javafx.base;
     opens  com.mateo.freetpv.controller to javafx.fxml;
